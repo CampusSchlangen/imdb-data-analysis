599503bf24d455153a823d2aa17697753868962a
Invincible2211 <58395689+Invincible2211@users.noreply.github.com>
2025-01-16 15:21:11 +0100

    initial Project Setup

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+    description = "Python development template";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = {
+        self,
+        nixpkgs,
+        utils,
+        ...
+    }:
+        utils.lib.eachDefaultSystem (system: let
+            pkgs = import nixpkgs {inherit system;};
+            pythonPkgs = pkgs.python312Packages;
+            devDeps = with pythonPkgs; [
+                pytest
+                black
+                isort
+            ];
+        in {
+            packages.default = pythonPkgs.buildPythonPackage {
+                pname = "text-adventure";
+                version = "0.1.0";
+                format = "pyproject";
+
+                src = ./.;
+
+                build-system = [pythonPkgs.hatchling];
+
+                dependencies = with pythonPkgs; [
+                    pynput
+                ];
+            };
+
+            devShells.default = pkgs.mkShell {
+                inputsFrom = [self.packages.${system}.default];
+                buildInputs = [devDeps];
+            };
+
+            checks = let
+                checkDeps = {
+                    buildInputs = [self.packages.${system}.default devDeps];
+                    src = ./.;
+                };
+            in {
+                tests = pkgs.runCommand "python-tests" checkDeps ''
+                    pytest $src
+                    touch $out
+                '';
+
+                format = pkgs.runCommand "python-format" checkDeps ''
+                    black --check $src
+                    isort --check-only $src
+                    touch $out
+                '';
+            };
+        });
+}
